5fe969d64df4397318b98e3980b6e399c9f89486
wk-j <somnuk.wk@gmail.com>
2023-06-10 11:03:32 +0700

    Add editor config, NuGet build and other files to the repository. Add C# code style settings.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Base 64
+
+Download content from URL then convert to base64 string.
+
+```bash
+dotnet tool install -g wk.Base64
+```
+
+# Usage
+
+```bash
+wk-base64 https://example-files.online-convert.com/document/txt/example.txt
+```
---
+++ src/Base64/Program.cs
@@ -0,0 +1,12 @@
+﻿if (args.Length == 0) {
+    Console.WriteLine("Please provide a URL to convert to Base64.");
+    return;
+}
+
+var url = args[0];
+
+using (var client = new HttpClient()) {
+    byte[] imageBytes = await client.GetByteArrayAsync(url);
+    var base64String = Convert.ToBase64String(imageBytes);
+    Console.WriteLine(base64String);
+}
